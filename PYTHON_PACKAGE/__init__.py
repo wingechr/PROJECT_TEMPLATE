3967a23a0a17ad8b005a1a11398b5b4df8a1e81f
c.winger <c.winger@oeko.de>
2024-06-26 15:29:13 +0200

    Bump version: 0.0.1 → 0.0.2

--- PYTHON_PACKAGE/__init__.py
+++ PYTHON_PACKAGE/__init__.py
@@ -1,5 +1,5 @@
 """MODULE DESCRIPTION
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = []
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         description="DESCRIPTION",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        version="0.0.1",
+        version="0.0.2",
         author="Christian Winger",
         author_email="c@wingechr.de",
         url="https://github.com/wingechr/PROJECT_TEMPLATE",
