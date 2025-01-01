e49b3acc925f999792931cb126867d45b1a6d7f6
Christian Winger <c@wingechr.de>
2025-01-01 17:02:34 +0100

    try to fix github actions

--- django_app/scripts/build.sh
+++ django_app/scripts/build.sh
@@ -3,12 +3,14 @@
 
 test -e venv || python -m venv venv
 . venv/bin/activate
-pip install -r django_app/requirements.txt
+pushd django_app
+pip install -r requirements.txt
 # node/npm
-npm install --no-save --omit=dev file:django_app
+npm install --no-save --omit=dev --prefix ..
 # copy default settings
-mkdir -p _local_data
-cp django_app/local_settings.example.py _local_data/local_settings.py
-python django_app/manage.py collectstatic --noinput
-python django_app/manage.py compress
-# TODO:translations python django_app/manage.py compilemessages
+mkdir -p ../_local_data
+cp local_settings.example.py ../_local_data/local_settings.py
+python manage.py collectstatic --noinput
+python manage.py compress
+# TODO:translations python manage.py compilemessages
+popd
