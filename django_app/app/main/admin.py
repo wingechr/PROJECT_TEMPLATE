25aa4d1269157bd91e665fcdc5a57e0875df9d2d
c.winger <c.winger@oeko.de>
2025-01-06 09:44:52 +0100

    api router

--- django_app/app/main/admin.py
+++ django_app/app/main/admin.py
@@ -1,15 +1,26 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-
-# from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 User = get_user_model()
 
-admin.site.site_header = settings.SITE_TITLE  # + " Admin"
+admin.site.site_header = settings.SITE_TITLE
 admin.site.site_title = settings.SITE_TITLE
 admin.site.index_title = settings.SITE_TITLE
 admin.site.site_url = None  # remove "View Site" link
 
 
-admin.site.register(User)
+# We need special user admin class to properly handle password encryption
+class UserAdmin(DefaultUserAdmin):
+    # add language in new section "Additional info"
+    fieldsets = DefaultUserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("language",)}),
+    )
+    # add language,email in creation
+    add_fieldsets = DefaultUserAdmin.add_fieldsets + (
+        (None, {"fields": ("language", "email")}),
+    )
+
+
+admin.site.register(User, UserAdmin)
